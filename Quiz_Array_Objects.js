043b68cbc6f76e784e0bc2fd377a72c035377932
MarkIsNotTaken <mdemesillo@gmail.com>
2016-12-07 08:37:20 -1000

    Quiz_Array_Objects.js

---
+++ Quiz_Array_Objects.js
@@ -0,0 +1,26 @@
+var myArray = ["Mark","Bob",10, boolean = true];
+
+
+function quiz(donut){
+	if(myArray[3] = true){
+		console.log("We are " + myArray[0] + " and " + myArray[1] + ", we are both " + myArray[2] + " years old.")
+	}else{
+		console.log("You shall not pass!")
+	}
+}
+
+quiz();
+
+var batman = {
+	man : true,
+	utilityBelt : ["Batarang", "Fist"],
+	vehicle : "batmobile",
+	sidekick : "robin"
+}
+
+function goth(cent){
+	if(batman.man = true){
+		console.log("I am the bat and I have " + batman.utilityBelt[0] + "," + batman.vehicle + " and " + batman.sidekick)
+	}
+}
+goth();
